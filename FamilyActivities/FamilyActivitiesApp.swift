cbe8a507c935311bc153040585ddb809ee2df2a1
Annalie <annaliakruseman@gmail.com>
2024-01-10 11:17:56 -0800

    Initial Commit

---
+++ FamilyActivities/FamilyActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyActivitiesApp.swift
+//  FamilyActivities
+//
+//  Created by Annalie Kruseman on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FamilyActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
