85878121362d324645c5e6d02fe610f25daa9b94
Annalie <annaliakruseman@gmail.com>
2024-01-31 13:05:16 -0800

    Change variable name in Friend

--- FamilyActivities/Friend.swift
+++ FamilyActivities/Friend.swift
@@ -11,30 +11,30 @@ import SwiftData
 @Model
 class Friend: Codable {
     enum CodingKeys: CodingKey {
-        case id, name, text
+        case id, name, someText
     }
 
     let id: String
     var name: String
-    var text: String
+    var someText: String
 
-    init(id: String, name: String, text: String) {
+    init(id: String, name: String, someText: String) {
         self.id = id
         self.name = name
-        self.text = text
+        self.someText = someText
     }
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(String.self, forKey: .id)
         name = try container.decode(String.self, forKey: .name)
-        text = try container.decode(String.self, forKey: .text)
+        someText = try container.decode(String.self, forKey: .someText)
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
         try container.encode(name, forKey: .name)
-        try container.encode(text, forKey: .text)
+        try container.encode(someText, forKey: .someText)
     }
 }
