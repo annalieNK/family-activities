8e1f71bf1a1612cd17bf535d839e562d5cfbcb5d
Annalie <annaliakruseman@gmail.com>
2024-02-01 18:50:43 -0800

    Working

--- FamilyActivities/RecommendationDetailView.swift
+++ FamilyActivities/RecommendationDetailView.swift
@@ -12,33 +12,42 @@ import SwiftUI
 
 struct RecommendationDetailView: View {
     @Environment(\.modelContext) var modelContext
- 
+    
     let recommendation: Recommendation
     
     var body: some View {
-            Form {
-                Section("About") {
-                    Text(recommendation.text)
-                        .padding(.vertical)
-                }
-                
-//                Section("Activities") {
-//                    List(recommendation.activitiesList, id: \.self) { activity in //id: \.self
-//                        NavigationLink {
-//                            RecommendationActivityDetailView(activity: activity)
-//                            //ActivityDetailView(activity: activity)
-//                            //Text(friend.type)
-//                        } label: {
-//                            Text(activity.name)
-//                        }
-//                    }
-//                }
+        //        NavigationView {
+        Form {
+            Section("About") {
+                Text(recommendation.text)
+                    .padding(.vertical)
             }
-            .listStyle(.grouped)
-            .navigationTitle(recommendation.name)
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Section("Activities") {
+//                List(recommendation.activitiesList, id: \.self) { activity in //id: \.self
+//                    NavigationLink(value: activity) {
+//                        RecommendationActivityDetailView(recommendationActivity: activity)
+//                        //ActivityDetailView(activity: activity)
+//                        //Text(activity.type)
+//                    } label: {
+//                    Text(activity.name)
+//                }
+                List(recommendation.activitiesList, id: \.self) { activity in
+                    NavigationLink(value: activity) {
+                        Text(activity.name)
+                    }
+                }
+                .navigationDestination(for: RecommendationActivity.self) { activity in
+                    RecommendationActivityDetailView(recommendationActivity: activity)
+            }
+        }
     }
-    
+        .listStyle(.grouped)
+        .navigationTitle(recommendation.name)
+        .navigationBarTitleDisplayMode(.inline)
+    //        }
+}
+
 }
 
 #Preview {
